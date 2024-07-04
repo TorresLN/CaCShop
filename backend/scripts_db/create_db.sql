3d69be64a88315bc152c1ddc6c1dfc89f957f8b5
Franco <fchavez@udc.edu.ar>
2024-07-04 19:47:00 -0300

    Cambio del campo Talles

--- backend/scripts_db/create_db.sql
+++ backend/scripts_db/create_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS productos(
     img VARCHAR(255) NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
     descripcion VARCHAR(255) NOT NULL,
-    talles CHAR(1) NOT NULL,
+    talles VARCHAR(100) NOT NULL,
     colores VARCHAR(50) NOT NULL,
     cantidad INT NOT NULL,
     destacado TINYINT(1) NOT NULL DEFAULT 0
