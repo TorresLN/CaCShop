4e76d0d45fa045feaa79a30f45223ec376d2a299
TorresLN <lautta.99@gmail.com>
2024-07-10 18:34:20 -0300

    Commit Gabriel

--- backend/src/config.example.py
+++ backend/src/config.example.py
@@ -1,7 +1,7 @@
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = '' #Nombre de usuario de la base de datos
+    MYSQL_HOST = '127.0.0.1'
+    MYSQL_USER = 'root' #Nombre de usuario de la base de datos
     MYSQL_PASSWORD = '' #Contraseña del usuario de base de datos
     MYSQL_DB = 'cac_shop_db' #Nombre de la base de datos
 
