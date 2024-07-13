8c2e47e081fb57040864bf754f8b1e6961a474c6
TorresLN <lautta.99@gmail.com>
2024-07-12 22:47:11 -0300

    Last commit

--- frontend/assets/js/editar_producto.js
+++ frontend/assets/js/editar_producto.js
@@ -121,9 +121,9 @@ formulario.addEventListener('submit', function (event) {
   )
   .then((response) => {
       if (response.ok) {
-        window.location.replace('intranet/admin.html?error=false');
+        window.location.replace('admin.html?error=false');
       } else {
-        window.location.replace('intranet/admin.html?error=true');
+        window.location.replace('admin.html?error=true');
         throw new Error('Error al agregar el producto.');
       }
   })
